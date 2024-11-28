28646e6627bb6c5049bdb582782cb61c20400130
Jayesh Pandey <100137975+jayesh-RN@users.noreply.github.com>
2024-11-28 12:57:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const hostname = '0.0.0.0';
 
 const port = 3000;
 
+const use = "for cicd";
+
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
