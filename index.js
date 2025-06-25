0ad8dafa59958e68a445a28587c9d02ed3d9b475
Jayesh Pandey <100137975+jayesh-RN@users.noreply.github.com>
2025-06-25 17:26:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const { createServer } = require('node:http');
 
-// change for checking pipeline ---------------------------------- let's see
-
 const hostname = '0.0.0.0';
-
-const port = 3000;
-
-const use = "For learning cicd triggers to pupeline take infinity + 1";
+const port = 80; // changed from 3000 to 80
 
 const server = createServer((req, res) => {
+    if (req.url === '/services/ping') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('pong');
+        return;
+    }
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World with test 10');
